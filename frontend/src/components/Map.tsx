7a2b7a1c82b5e7329dc7c6ae338e2062a3054e6c
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-20 19:08:05 +1200

    add mapbox map

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,9 +1,31 @@
+import { useRef, useEffect, useState } from 'react'
+
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 import '../App.css'
 
 export default function Map() {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapRef.current = new mapboxgl.Map({
+      accessToken: import.meta.env.VITE_DEVELOPMENT_TOKEN,
+      container: mapContainerRef.current,
+
+      center: [174.76, -36.86], 
+      zoom: 11.4
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  });
+
   return (
     <>
-      <div id='map-container'></div>
+      <div id='map-container' ref={mapContainerRef}></div>
     </>
   );
 }
