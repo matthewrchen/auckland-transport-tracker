3be4e709b2637a650a58661bf0143ed3ba53c456
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-25 17:15:43 +1200

    add FastAPI

---
+++ backend/src/app/fastapi/manager.py
@@ -0,0 +1,14 @@
+from fastapi import WebSocket
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: set[WebSocket] = set()
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.add(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+websocket_manager = ConnectionManager()
