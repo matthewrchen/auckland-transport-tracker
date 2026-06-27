85193744834d173004cd09cd8e90f70f1e59b6ac
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-27 21:02:31 +1200

    rename database table

--- backend/src/app/database/repository.py
+++ backend/src/app/database/repository.py
@@ -7,16 +7,16 @@ class ATRepository:
         self.database = supabase_client
     
     async def upsert_vehicle_locations(self, data: list):
-        response = await (self.database.table("Vehicle Positions")
+        response = await (self.database.table("realtime_vehicles")
                     .upsert(data).execute())
         
     async def clean_vehicle_locations(self):
         cutoff = datetime.now(timezone.utc) - timedelta(minutes=1)
         cutoff_iso = cutoff.isoformat()
-        response = await (self.database.table("Vehicle Positions")
+        response = await (self.database.table("realtime_vehicles")
                     .delete().lt("last_updated", cutoff_iso).execute())
         
     async def get_vehicle_locations(self) -> list:
-        response = await (self.database.table("Vehicle Positions")
+        response = await (self.database.table("realtime_vehicles")
                     .select("*").execute())
         return response.data
