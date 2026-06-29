56a09211d1692fa16045417c8f77c30b9aac58a1
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-29 16:59:52 +1200

    add parsing for trip update entities

--- backend/src/app/transformers/transformer.py
+++ backend/src/app/transformers/transformer.py
@@ -8,7 +8,13 @@ def parse_vehicle_location_data(data: list) -> list:
         # if label is AMP ### and license plate empty, vehicle is train
 
         if (entity.get("trip_update")):
-            nested_entity = entity.get("trip_update")
+            nested_entity = entity["trip_update"]
+            parsed_entity = {
+                "trip_id": nested_entity["trip"]["trip_id"],
+                "route_id": nested_entity["trip"]["route_id"],
+                "updated_at": update_time,
+                "raw_data": nested_entity
+            }
         elif (entity.get("vehicle")):
             # TODO add more fallbacks later
 
