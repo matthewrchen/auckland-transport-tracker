d97326b8f42c9f18f4fb82139b567112ba74f006
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-07-09 16:08:43 +1200

    add database operations for combined feed

--- backend/src/app/backend_api/server.py
+++ backend/src/app/backend_api/server.py
@@ -32,18 +32,18 @@ async def vehicle_polling(app: FastAPI):
     while True:
         try:
             print("[POLLER] Making AT request")
-            raw_data = await app.state.at_client.get_vehicle_updates()
+            raw_data = await app.state.at_client.get_combined_feed()
             transformed_data = transformers.transformer.parse_realtime_data(raw_data)
             
             print("[POLLER] Updating database")
-            await app.state.repository.upsert_realtime_vehicles(transformed_data[1])
+            await app.state.repository.upsert_realtime_data(transformed_data)
 
             print("[POLLER] Sending data to users")
             latest_data = await app.state.repository.get_view_realtime_vehicles()
             payload = {"type": "live_update", "vehicles": latest_data}
             await websocket_manager.broadcast_to_all(payload)
 
-            await app.state.repository.clean_realtime_vehicles()
+            await app.state.repository.clean_realtime_data()
         except asyncio.CancelledError:
             break  
         except Exception as e:
--- backend/src/app/database/repository.py
+++ backend/src/app/database/repository.py
@@ -1,34 +1,52 @@
 from supabase import AsyncClient
-
+import asyncio
 from datetime import datetime, timedelta, timezone
 
 class ATRepository:
     def __init__(self, supabase_client: AsyncClient):
         self.database = supabase_client
 
-    async def upsert_realtime_trips(self, data: list):
+    async def _upsert_realtime_trips(self, data: list):
         response = await (self.database.table("realtime_trips")
                     .upsert(data).execute())
     
-    async def upsert_realtime_vehicles(self, data: list):
+    async def _upsert_realtime_vehicles(self, data: list):
         response = await (self.database.table("realtime_vehicles")
                     .upsert(data).execute())
-        
-    async def clean_realtime_vehicles(self):
-        cutoff = datetime.now(timezone.utc) - timedelta(minutes=1)
-        cutoff_iso = cutoff.isoformat()
-        response = await (self.database.table("realtime_vehicles")
-                    .delete().lt("updated_at", cutoff_iso).execute())
-        
-    async def get_realtime_vehicles(self) -> list:
-        response = await (self.database.table("realtime_vehicles")
-                    .select("*").execute())
-        return response.data
-    
-    async def upsert_realtime_alerts(self, data: list):
+
+    async def _upsert_realtime_alerts(self, data: list):
         response = await (self.database.table("realtime_alerts")
                     .upsert(data).execute())
         
+    async def _clean_realtime_trips(self, cutoff_iso):
+        response = await (self.database.table("realtime_trips")
+                    .delete().lt("updated_at", cutoff_iso).execute())
+        
+    async def _clean_realtime_vehicles(self, cutoff_iso):
+        response = await (self.database.table("realtime_vehicles")
+                    .delete().lt("updated_at", cutoff_iso).execute())
+    
+    async def _clean_realtime_alerts(self, cutoff_iso):
+        response = await (self.database.table("realtime_alerts")
+                    .delete().lt("updated_at", cutoff_iso).execute())
+    
+    async def upsert_realtime_data(self, data: list):
+        await asyncio.gather(
+            self._upsert_realtime_trips(data[0]),
+            self._upsert_realtime_vehicles(data[1]),
+            self._upsert_realtime_alerts(data[2])
+        )
+
+    async def clean_realtime_data(self):
+        cutoff = datetime.now(timezone.utc) - timedelta(minutes=1)
+        cutoff_iso = cutoff.isoformat()
+
+        await asyncio.gather(
+            self._clean_realtime_trips(cutoff_iso),
+            self._clean_realtime_vehicles(cutoff_iso),
+            self._clean_realtime_alerts(cutoff_iso)
+        )
+
     async def get_view_realtime_vehicles(self):
         response = await (self.database.table("view_realtime_vehicles")
                     .select("*").execute())
